fea6f383d5e91c4551de253a24c981c0082e4f4b
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2022-07-23 18:59:15 +0300

    [main] add html partials

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,34 +1,56 @@
-const Path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Path = require("path");
+const fs = require("fs");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+function generateHtmlPlugins(templateDir) {
+  const templateFiles = fs.readdirSync(Path.resolve(__dirname, templateDir));
+  return templateFiles.map((item) => {
+    const parts = item.split(".");
+    const name = parts[0];
+    const extension = parts[1];
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: Path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+      inject: false,
+    });
+  });
+}
+
+const htmlPlugins = generateHtmlPlugins("../src/html/views");
 
 module.exports = {
-  entry: {
-    app: Path.resolve(__dirname, '../src/scripts/index.js'),
-  },
+  entry: ["./src/scripts/index.js", "./src/styles/index.scss"],
   output: {
-    path: Path.join(__dirname, '../build'),
-    filename: 'js/[name].js',
+    path: Path.join(__dirname, "../build"),
+    filename: "js/[name].js",
   },
   optimization: {
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
       name: false,
     },
   },
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
-      patterns: [{ from: Path.resolve(__dirname, '../public'), to: 'public' }],
+      patterns: [
+        { from: Path.resolve(__dirname, "../public"), to: "public" },
+        {
+          from: "./src/assets/fonts",
+          to: "./fonts",
+        },
+        {
+          from: "./src/assets/images",
+          to: "./images",
+        },
+      ],
     }),
-    new HtmlWebpackPlugin({
-      template: Path.resolve(__dirname, '../src/index.html'),
-    }),
-  ],
+  ].concat(htmlPlugins),
   resolve: {
     alias: {
-      '~': Path.resolve(__dirname, '../src'),
+      "~": Path.resolve(__dirname, "../src"),
     },
   },
   module: {
@@ -36,15 +58,12 @@ module.exports = {
       {
         test: /\.mjs$/,
         include: /node_modules/,
-        type: 'javascript/auto',
+        type: "javascript/auto",
       },
       {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
-        type: 'asset'
+        test: /\.html$/,
+        include: Path.resolve(__dirname, "../src/html/partials"),
+        use: ["raw-loader"],
       },
     ],
   },
