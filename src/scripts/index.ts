e4a134f06d0556152d9a589eecb75e385cd41607
Evgeniy Ponomarev <ponamarevevgeniy11@gmail.com>
2022-08-02 22:43:01 +0300

    update

---
+++ src/scripts/index.ts
@@ -0,0 +1,3 @@
+import "../styles/index.scss";
+
+((): void => console.log("webpack starterkit"))();
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -8,11 +8,12 @@ function generateHtmlPlugins(templateDir) {
   const templateFiles = fs.readdirSync(Path.resolve(__dirname, templateDir));
   return templateFiles.map((item) => {
     const parts = item.split(".");
-    const name = parts[0];
-    const extension = parts[1];
+    const name = parts.slice(0, -1).join(".");
+    const extension = parts.at(-1);
     return new HtmlWebpackPlugin({
-      filename: `${name}.html`,
+      filename: `${name.replace(".", "/")}.html`,
       template: Path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
+      minify: true,
     });
   });
 }
@@ -21,11 +22,11 @@ const htmlPlugins = generateHtmlPlugins("../src/html/views");
 
 module.exports = {
   entry: {
-    app: Path.resolve(__dirname, "../src/scripts/index.js"),
+    app: Path.resolve(__dirname, "../src/scripts/index.ts"),
   },
   output: {
     path: Path.join(__dirname, "../build"),
-    filename: "js/[name].js",
+    filename: "js/[name].[hash].js",
   },
   optimization: {
     splitChunks: {
@@ -47,9 +48,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: "javascript/auto",
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.ejs$/i,
@@ -60,8 +61,25 @@ module.exports = {
         },
       },
       {
-        test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
+        test: /\.(woff2?|ttf|eot)(\?v=\w+)?$/,
         type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext][query]",
+        },
+      },
+      {
+        test: /\.(ico|jpg|jpeg|png|gif|webp|svg)(\?.*)?$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][hash][ext][query]",
+        },
+      },
+      {
+        test: /\.(ico|svg)(\?.*)?$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/icons/[name][hash][ext][query]",
+        },
       },
     ],
   },
